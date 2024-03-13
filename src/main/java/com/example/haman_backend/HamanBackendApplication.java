b95dd4939ca61d5d9b50238d0befcf4f7b0f46a5
Time_ON <haas.timeon@gmail.com>
2024-03-13 16:34:40 +0100

    Initial commit

---
+++ src/main/java/com/example/haman_backend/HamanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.haman_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamanBackendApplication.class, args);
+    }
+
+}
